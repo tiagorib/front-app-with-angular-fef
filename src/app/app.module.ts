441490ac753e27f17e82e921a0e1e6dc8b2810ef
Tiago Ribeiro Carneiro <tiago.rib@gmail.com>
2023-06-14 17:38:22 -0300

    feat: create menubar and components for category and products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,11 +22,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CustomerComponent } from './customer/customer.component';
 import { CustomerService } from './service/customer.service';
 import { DatePipe } from '@angular/common';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
+import { CategoryService } from './service/category.service';
+import { ProductService } from './service/product.service';
+import { HomeComponent } from './home/home.component';
+import { MenubarComponent } from './menubar/menubar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent
+    CustomerComponent,
+    CategoryComponent,
+    ProductComponent,
+    HomeComponent,
+    MenubarComponent
   ],
   imports: [
     BrowserModule,
@@ -41,11 +53,15 @@ import { DatePipe } from '@angular/common';
     MatCardModule,
     MatTableModule,
     MatPaginatorModule,
+    MatMenuModule,
+    MatSelectModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
   providers: [
     CustomerService,
+    CategoryService,
+    ProductService,
     DatePipe
   ],
   bootstrap: [AppComponent]
