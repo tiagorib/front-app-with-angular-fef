16e475fd35069d5837f8e69e6d38da52a3b973c2
Lucazay <lucasay2003@gmail.com>
2023-06-14 19:40:44 -0300

    category

---
+++ src/app/model/product.ts
@@ -0,0 +1,15 @@
+import { Category } from "./category";
+
+export interface Product{
+    idProduct?: any;
+    nameProduct: string;
+    descriptionProduct: string;
+    skuProduct: any;
+    eanProduct: any;
+    costPriceProduct: string;
+    amountProduct: string;
+    dateCreatedProduct: any;
+    publishedProduct: boolean;
+    
+}
+
--- src/app/service/category.service.ts
+++ src/app/service/category.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Category } from '../model/category';
+import { Observable } from 'rxjs';
+import { API_CONFIG } from '../config/api_config';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
-}
+  url: string = API_CONFIG.urlApi;
+
+  constructor(private http: HttpClient) { }
+
+  save(category: Category) : Observable<Category[]> {
+    return this.http.post<Category[]>(this.url+'/category/insert', category);    
+  }
+
+  list() : Observable<Category[]> {
+    return this.http.get<Category[]>(this.url+'/category/list');    
+  }
+
+  delete(idCategory: any): Observable<Category> {
+    return this.http.delete<Category>(`${this.url}/category/delete/${idCategory}`);
+  }
+
+  findById(idCategory: any): Observable<Category> {
+    return this.http.get<any>(`${this.url}/category/findCategory/${idCategory}`);
+  }}
