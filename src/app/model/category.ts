58d131968a00f63121f2b16b555cdb38aa4c9ec3
Admin <joao.lucas280398@gmail.com>
2023-06-14 22:11:40 -0300

    av2-lpvv

---
+++ src/app/model/category.ts
@@ -0,0 +1,6 @@
+export interface Category{
+
+    idCategory?: any;
+    nameCategory: string;
+    descriptionCategory: string;
+}
---
+++ src/app/model/product.ts
@@ -0,0 +1,19 @@
+import { Category } from "./category";
+
+export interface Product{
+
+    idProduct?: any;
+    nameProduct: string;
+    descriptionProduct: string;
+    skuProduct: any;
+    eanProduct: any;
+    costPriceProduct: string;
+    amountProduct: string;
+    publishedProduct: boolean;
+    dateCreatedProduct: any;
+    idCategory:any;
+    
+
+   
+}
+
