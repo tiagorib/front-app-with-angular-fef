e288103436641e9a0dfc3d2bdb5d14ddc528c4c6
caahhcarvalho <ana.carvalhosi2021@gmail.com>
2023-06-14 21:18:05 -0300

    Implementação da category

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Category } from '../model/category';
+import { CategoryService } from '../service/category.service';
 
 @Component({
   selector: 'app-category',
@@ -7,4 +11,58 @@ import { Component } from '@angular/core';
 })
 export class CategoryComponent {
 
+  displayedColumns: string[] = ['id_category', 'name_category', 'description_category', 'deleteCategory', 'findCategory'];
+  ELEMENT_DATA: Category[] = [];
+  message: string = '';
+  dataSource = new MatTableDataSource<Category>(this.ELEMENT_DATA);
+  success: boolean = false;
+  errors!: String[];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private service: CategoryService){   
+  }
+
+  ngOnInit(): void {   
+    this.listCategory(); 
+  }
+
+  category: Category = {
+    idCategory: '',
+    nameCategory:'' ,
+    descriptionCategory:'' ,
+  }
+
+  saveCategory() {    
+    this.service.save(this.category).subscribe((response: any) => {
+      this.success = true;
+      this.errors = [];
+      this.category = response.result as Category;       
+      this.listCategory();   
+    });
+  }
+
+  listCategory() {
+    this.service.list().subscribe((response: any) => {
+      this.ELEMENT_DATA = response.result as Category[];
+      this.dataSource = new MatTableDataSource<Category>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  deleteCategory(category: Category) {
+    if (window.confirm('Deseja realmente excluir esta categoria?')) {
+      this.service.delete(category.idCategory).subscribe((response: any) => {
+        this.message = response.result.result as string;
+        window.alert(this.message);
+        this.listCategory();
+      });
+    }
+  }
+
+  findCategory(category: Category) {    
+    this.service.findById(category.idCategory).subscribe((response: any) => {
+      this.category = response.result as Category; 
+    });
+  }
 }
--- src/app/service/category.service.ts
+++ src/app/service/category.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { API_CONFIG } from '../config/api_config';
+import { Observable } from 'rxjs';
+import { Category } from '../model/category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
+  url: string = API_CONFIG.urlApi;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  save(category: Category) : Observable<Category[]> {
+    return this.http.post<Category[]>(this.url+'/category/insert', category);    
+  }
+
+  list() : Observable<Category[]> {
+    return this.http.get<Category[]>(this.url+'/category/list');    
+  }
+
+  delete(idCategory: any): Observable<Category> {
+    return this.http.delete<Category>(`${this.url}/category/delete/${idCategory}`);
+  }
+
+  findById(idCategory: any): Observable<Category> {
+    return this.http.get<any>(`${this.url}/category/findCategory/${idCategory}`);
+  }
 }
