b3710bb2569cf51fb21774e09d6e7a29930c2b7c
caahhcarvalho <ana.carvalhosi2021@gmail.com>
2023-06-14 21:44:09 -0300

    Implementacao Product

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,10 +1,123 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Product } from '../model/product';
+import { ProductService } from '../service/product.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Category } from '../model/category';
+import { DatePipe } from '@angular/common';
+import { CategoryService } from '../service/category.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+
+
+export class ProductComponent implements OnInit{
+  
+  displayedColumns: string[] = ['idProduct', 'nameProduct', 'descriptionProduct','categoryId', 'skuProduct', 'eanProduct', 'costPriceProduct', 'amountProduct','dateCreatedProduct', 'publishedProduct','deleteProduct','findProduct'];
+  ELEMENT_DATA: Product[] = [];
+  ELEMENT_DATA_CATEGORY: Category[] = [];
+  message: string = '';
+  dataSource = new MatTableDataSource<Product>(this.ELEMENT_DATA);
+
+  success: boolean = false;
+  errors!: String[];
+  
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  
+
+  product: Product = {
+  idProduct: '',
+  nameProduct: '',
+  descriptionProduct: '',
+  skuProduct: '',
+  eanProduct: '',
+  costPriceProduct: '',
+  amountProduct: '',
+  publishedProduct: false,
+  dateCreatedProduct: '',
+  idCategory: 3
+  }
+  constructor(private productService:ProductService){
+    
+  }
+  ngOnInit(): void {
+    this.listProduct();
+  }
+
+  public saveProduct(){
+    const datePipe = new DatePipe('en-US');
+    this.product.dateCreatedProduct = datePipe.transform(this.product.dateCreatedProduct, 'dd/MM/yyyy');
+    if (this.product.idProduct== ""){
+    this.productService.save(this.product).subscribe({next: response =>{
+      this.success = true;
+      this.errors = [];
+      this.listProduct();    
+    }, error: ex => {
+      if (ex.error.errors) {
+        this.errors = ex.error.errors;
+        this.success = false;
+        ex.error.errors.forEach((element:any) => {         
+        });
+      } else {
+          this.success = false;
+          this.errors = ex.error.errors;        
+      }
+    }})}
+    else{
+      this.productService.update(this.product).subscribe({next: response =>{
+      this.success = true;
+      this.errors = [];    
+      this.listProduct();    
+    }, error: ex => {
+      if (ex.error.errors) {
+        this.errors = ex.error.errors;
+        this.success = false;
+        ex.error.errors.forEach((element:any) => {         
+        });
+      } else {
+          this.success = false;
+          this.errors = ex.error.errors;        
+      }
+    }})
+    }
+  }
+
+  public listProduct() {
+    this.productService.list().subscribe((response: any) => {
+      this.ELEMENT_DATA = response.result as Product[];
+      this.dataSource = new MatTableDataSource<Product>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  listCategory() {
+    this.productService.listCategory().subscribe((response: any) => {
+      this.ELEMENT_DATA_CATEGORY = response.result as Category[];
+    });
+  }
+
+  deleteProduct(product: Product) {
+    if (window.confirm('Deseja realmente excluir este cliente?')) {
+      this.productService.delete(product.idProduct).subscribe((response: any) => {
+        this.message = response.result.result as string;
+        window.alert(this.message);
+        this.listProduct();
+      });
+    }
+  }
+
+  findProduct(product: Product) {    
+    this.productService.findById(product.idProduct).subscribe((response: any) => {
+      this.product = response.result as Product;
+      const datePipe = new DatePipe('en-US');
+      var date = this.product.dateCreatedProduct;
+      var newDate = date.split("/").reverse().join("-");
+      this.product.dateCreatedProduct = newDate;
+    });
+  }
 
 }
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { Product } from '../model/product';
+import { API_CONFIG } from '../config/api_config';
+import { Category } from '../model/category';
+
+@Injectable()
 export class ProductService {
+    url = API_CONFIG.urlApi
+  constructor(private http: HttpClient) {}
+  save (product: Product) : Observable<Product[]> {
+    return this.http.post<Product[]>(this.url+'/product/insert', product);
+  }
 
-  constructor() { }
+  list() : Observable<Product[]>{
+  return this.http.get<Product[]>(this.url+'/product/list');
+}
+
+  listCategory() : Observable<Category[]>{
+  return this.http.get<Category[]>(this.url+'/category/list');
+}
+
+  delete(idProduct:any) : Observable<Product[]>{
+  return this.http.delete<Product[]>(`${this.url}/product/delete/${idProduct}`);
+}
+
+  findById(idProduct: any): Observable<Product> {
+  return this.http.get<any>(`${this.url}/product/findProduct/${idProduct}`);
+}
+
+  update(product: Product) : Observable<Product>{
+    return this.http.put<Product>(this.url+'/product/update', product);
+  }
+
 }
