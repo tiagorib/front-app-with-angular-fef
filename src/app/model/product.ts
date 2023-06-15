4448b130ad63a2569f36302bcf7c7477cb27bb54
brunocduran <brunocesarduran@gmail.com>
2023-06-14 21:42:47 -0300

    feat: av2-bruno

---
+++ src/app/model/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    idProduct?: any;
+    nameProduct: string;
+    descriptionProduct: string;
+    skuProduct: any;
+    eanProduct: any;
+    costPriceProduct: any;
+    amountProduct: any;
+    publishedProduct: boolean;
+    dateCreatedProduct: any;
+    idCategory: any;
+}
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Product } from '../model/product';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { ProductService } from '../service/product.service';
+import { DatePipe } from '@angular/common';
+import { Category } from '../model/category';
+import { CategoryService } from '../service/category.service';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +13,87 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
+
+
+  displayedColumns: string[] = ['idProduct', 'nameProduct', 'descriptionProduct', 'category', 'skuProduct', 'eanProduct', 'costPriceProduct', 'publishedProduct', 'deleteProduct', 'findProduct'];
+  ELEMENT_DATA: Product[] = [];
+  message: string = '';
+  dataSource = new MatTableDataSource<Product>(this.ELEMENT_DATA);
+  success: boolean = false;
+  errors!: String[];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(
+    private productService: ProductService,
+    private categoryService: CategoryService){   
+  }
+
+  ngOnInit(): void {   
+    this.listProduct(); 
+    this.findAllCategory();
+  }
+
+  product: Product = {
+    idProduct: '',
+    nameProduct: '',
+    descriptionProduct: '',
+    skuProduct: '',
+    eanProduct: '',
+    costPriceProduct: '',
+    amountProduct: '',
+    publishedProduct: true,
+    dateCreatedProduct: '',
+    idCategory: ''
+  }
+
+  categorys: Category[] = []
+
+  findAllCategory() {
+    this.categoryService.list().subscribe((response: any) => {
+      this.categorys = response.result;
+    })
+  }
+
+
+
+  saveProduct() {    
+    const datePipe = new DatePipe('en-US');
+    this.product.dateCreatedProduct = datePipe.transform(this.product.dateCreatedProduct, 'dd/MM/yyyy');
+    
+    this.productService.save(this.product).subscribe((response: any) => {
+      this.success = true;
+      this.errors = [];
+      this.product = response.result as Product;       
+      var date = this.product.dateCreatedProduct;
+      var newDate = date.split("/").reverse().join("-");
+      this.product.dateCreatedProduct = newDate; 
+      this.listProduct();   
+    });
+  }
+
+  listProduct() {
+    this.productService.list().subscribe((response: any) => {
+      this.ELEMENT_DATA = response.result as Product[];
+      this.dataSource = new MatTableDataSource<Product>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  deleteProduct(product: Product) {
+    if (window.confirm('Deseja realmente excluir este produto?')) {
+      this.productService.delete(product.idProduct).subscribe((response: any) => {
+        this.message = response.result.result as string;
+        window.alert(this.message);
+        this.listProduct();
+      });
+    }
+  }
+
+  findProduct(product: Product) {    
+    this.productService.findById(product.idProduct).subscribe((response: any) => {
+      this.product = response.result as Product; 
+    });
+  }
 
 }
